c302692231496057c2f93f9ceb4a4245387caf3a
Emeric Robin <70618405+Emeric-bas@users.noreply.github.com>
2021-09-30 19:34:41 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,24 @@
-console.log(allo)
+const chaptersObj = {
+    sortie: {
+        subtitle: 'Première sortie',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        img: 'ete.png',
+        options: [
+            "avancer vers l'inconnu" = {
+              text: "avancer vers l'inconnu",  
+              action: goToChapter(rencontre)
+            }
+        ]
+    },
+    rencontre: {
+        subtitle: 'Chasse la licorne',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        img: 'ete.png'
+    }
+}
+
+function goToChapter(nomChapitre){
+    const chapter = chaptersObj[nomChapitre];
+    console.log(chapter.subtitle);
+    console.log(chapter.subtitle);
+}
