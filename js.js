0ca90c8aa554a429e6acf09f993b4c6436ff5817
Emeric Robin <70618405+Emeric-bas@users.noreply.github.com>
2021-09-30 19:43:14 -0400

    premieres options

--- js.js
+++ js.js
@@ -13,8 +13,40 @@ const chaptersObj = {
     rencontre: {
         subtitle: 'Chasse la licorne',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
-        img: 'ete.png'
-    }
+        img: 'ete.png',
+        options: [
+            "Tirer la licorne" = {
+              text: "Tirer la licorne",  
+              action: goToChapter(mise_a_mort)
+            },
+            "La laisser partir" = {
+                text: "Vous refuser de tirer mais votre maitre la tue",  
+                action: goToChapter(la_part_du_maitre)
+            }
+        ]
+    },
+    mise_a_mort: {
+        subtitle: 'Mise à mort',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        img: 'ete.png',
+        options: [
+            "Retour à la ville" = {
+              text: "Retour à la ville",  
+              action: goToChapter(marchandage)
+            }
+        ]
+    },
+    la_part_du_maitre: {
+        subtitle: 'Mise à mort',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+        img: 'ete.png',
+        options: [
+            "Retour à la ville" = {
+              text: "Retour à la ville",  
+              action: goToChapter(marchandage)
+            }
+        ]
+    },
 }
 
 function goToChapter(nomChapitre){
